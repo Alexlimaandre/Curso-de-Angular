ee2c2362c9a13fe119d2043e742d8f5ef779f930
Alexlimaandre <alexandre.limaandre@gmail.com>
2026-04-07 21:28:28 -0300

    Aula 05 -  Interpolação de Dados

--- Angular/src/app/components/first-component/first-component.ts
+++ Angular/src/app/components/first-component/first-component.ts
@@ -2,8 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-first-component',
-  imports: [],
+  imports: [],  
   templateUrl: './first-component.html',
   styleUrl: './first-component.css',
 })
-export class FirstComponent {}
+export class FirstComponent {
+
+  name: string = "Alexandre";
+  idade: number = 19;
+  job : string = "Programador";
+  hobbies: string[] = ['Jogar', 'Estudar', 'Treinar'];
+  jogo = {
+    name: 'Rocket League',
+    year: '2015',
+    descricao: 'Jogo de carro com futebol'
+  }
+
+  constructor(){
+
+  }
+
+}
