12767139d7b6c73cba8f2d52abae03b98c2c837d
Alexandre Lima <alexandre.limaandre@gmail.com>
2026-04-10 16:52:26 -0300

    Aula 06 - CSS no Angular

--- Angular/src/app/app.ts
+++ Angular/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FirstComponent } from './components/first-component/first-component';
+import { Home } from './components/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FirstComponent],
+  imports: [RouterOutlet, FirstComponent, Home ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
