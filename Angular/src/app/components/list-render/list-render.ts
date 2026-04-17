7e3b79346f21fa24abfb6fb92f7bb7d41ef82ff1
Alexandre Lima <alexandre.limaandre@gmail.com>
2026-04-17 09:14:57 -0300

    Aula 12 - Renderização de Listas

--- Angular/src/app/components/list-render/list-render.ts
+++ Angular/src/app/components/list-render/list-render.ts
@@ -10,10 +10,18 @@ export class ListRender {
 
   // Por padrão os dados vem de um banco, mas para a prática veremos esse dados mocado
   animals: any = [
-    {name: "Bidu", type: "Dog"},
+    {name: "Bidu", Type: "Dog"},
     {name: "Tom", Type: "Cat"},
     {name: "Frida", Type: "Fish"},
+    {name: "Clóvis", Type: "Bird"},
+    // for(){}, no formato atuualm oide gerar algumu problemaa
     {name: "Clóvis", Type: "Bird"}
   ]
+
+  names : any = [ {id: 1, name: "Alex", ismy: "Eu"},
+    {id: 2, name: "Pedro", ismy: "Brother"},
+    {id: 3, name: "Gabi", ismy: "Sister"},
+    {id: 4, name: "Ana", ismy: "Mother"},
+    {id: 5, name: "Clóvis", ismy: "Grandmother"}]
 
 }
